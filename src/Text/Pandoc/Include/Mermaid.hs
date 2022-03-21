11efa8d4af98fe0ae67670272a447c94a1e66ca0
Hasan al Rasyid <hasanalrasyid@home>
2022-03-21 09:50:54 +0900

    we have relative size for mermaid

--- src/Text/Pandoc/Include/Mermaid.hs
+++ src/Text/Pandoc/Include/Mermaid.hs
@@ -10,18 +10,21 @@ import Text.Pandoc.Definition
 import Data.Maybe
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import qualified Data.Text.Read as T
+import Data.Either
 
 doInclude :: Block -> IO Block
 doInclude (CodeBlock (label, ["mermaid"], opts) md) = do
-  let fileName = case lookup "file" opts of
-                    Nothing -> "mermaid"
-                    Just a -> T.unpack a
-      width  = fromMaybe "800" $ lookup "width" opts
-      height = fromMaybe "600" $ lookup "height" opts
-  T.writeFile ("_build/temp/" ++ fileName ++ ".mmd") md
-  _ <- readProcess "mermaid" ["-w", T.unpack width,"-H", T.unpack height,"-i", "_build/temp/" ++ fileName ++ ".mmd", "-o", "_build/auto/" ++ fileName ++ ".png" ] []
-  let caption = fromMaybe "No caption" $ lookup "caption" opts
-  return $ Div nullAttr [ Para [Image (label,[],opts) [Str caption] ((T.pack $ fileName ++ ".png"),label)]]
+  let fileName = fromMaybe "mermaid" $ lookup "file" opts
+      size = fromRight 0.8  $ fmap fst $ fromMaybe (Right (0.8,"")) $ T.double <$> lookup "size" opts
+      width = floor $ 2480.0 * size
+      height = floor $ (fromIntegral $ 600 * width) / 800.0
+  T.writeFile (T.unpack $ "_build/temp/" <> fileName <> ".mmd") md
+  callCommand $ unwords [ "mermaid -f -w", show width,"-H", show height," -i _build/temp/" <> T.unpack fileName <> ".mmd -o _build/auto/" <> T.unpack fileName <> ".pdf" ]
+  let caption = case lookup "caption" opts of
+                  Nothing -> []
+                  Just a -> [Str a]
+  return $ Div nullAttr [ Para [Image (label,[],opts) caption (fileName,label)]]
 
 doInclude x = return x
 
